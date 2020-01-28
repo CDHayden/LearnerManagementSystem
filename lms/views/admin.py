10d795d30f7c6c67586c256e8cd63467827505c6
CD Hayden <chayden50@gmail.com>
2020-01-28 15:09:27 +0000

    Create admin index page

--- lms/views/admin.py
+++ lms/views/admin.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
 
+from database import mongo
+
 mod = Blueprint("admin", __name__)
 
 @mod.route('/')
 def admin_index():
-    return render_template('admin/index.html')
+    return render_template('admin/index.html', user = mongo.db.users.find_one())
