25b1dfbf0905944e3ea0b021ad395e65a414d9d6
CD Hayden <chayden50@gmail.com>
2020-01-28 15:05:49 +0000

    Create staff index page

--- lms/views/staff.py
+++ lms/views/staff.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
 
+from database import mongo
+
 mod = Blueprint("staff", __name__)
 
 @mod.route('/')
 def staff_index():
-    return render_template('staff/index.html')
+    return render_template('staff/index.html', user = mongo.db.users.find_one())
