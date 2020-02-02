99ef25bcdcb4a7a8ae0887ab8b2d1244f589b831
CD Hayden <chayden50@gmail.com>
2020-02-02 12:52:46 +0000

    Add functions to generate side menu of Subjects and classes

--- lms/views/student.py
+++ lms/views/student.py
@@ -4,10 +4,42 @@ from database import mongo
 
 mod = Blueprint("student", __name__)
 
+
+def get_class_subject(class_name):
+    """Return the subject that class_name belongs to"""
+    selected_class = mongo.db.classes.find_one({'name': class_name})
+    return selected_class['subject']
+
+
+def get_subjects_classes(student):
+    """Return a dict of subjects and their classes for given student
+
+    Keyword arguements:
+    student -- The student you want subject and class information of
+    """
+    subjects_classes = {}
+    for current_class in student['classes']:
+        subject = get_class_subject(current_class['name'])
+        if subject not in subjects_classes:
+            subjects_classes[subject] = []
+        subjects_classes[subject].append(current_class['name'])
+    return subjects_classes
+
+
 @mod.route('/')
 def student_index():
-    return render_template('student/index.html', student = mongo.db.users.find_one({'forename':'Chris'}))
+    student = mongo.db.users.find_one({'forename': 'Chris'})
+    return render_template('student/index.html', student=student)
+
 
 @mod.route('/classes')
 def student_classes():
-    return render_template('student/classes.html', student = mongo.db.users.find_one({'forename':'Chris'}))
+    student = mongo.db.users.find_one({'forename': 'Chris'})
+    menu_items = get_subjects_classes(student)
+    return render_template('student/classes.html',
+                           student=student,
+                           menu_items=menu_items)
+
+
+# @mod.route('/classes/<class_name>')
+# def class_content(class_name):
