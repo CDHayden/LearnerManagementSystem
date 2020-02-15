d2a0c00efce0faf9355f1822bedc7aaa9d819c71
CD Hayden <chayden50@gmail.com>
2020-02-15 18:33:16 +0000

    Use .env file instead of config.py to load variables

--- lms/__init__.py
+++ lms/__init__.py
@@ -1,23 +1,26 @@
 import os
 
-from database import mongo
 from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
 
+from database import mongo
 from .views.home import mod as home
 from .views.student import mod as student
 from .views.admin import mod as admin
 from .views.staff import mod as staff
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
+#Load environment variables
+load_dotenv()
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get("SECRET_KEY")
 app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
-# app.config.from_pyfile('config.py')
+
 mongo.init_app(app)
 
 app.register_blueprint(home)
 app.register_blueprint(student, url_prefix="/student")
 app.register_blueprint(admin, url_prefix="/admin")
 app.register_blueprint(staff, url_prefix="/staff")
-
-# app.secret_key = app.config['SECRET_KEY']
-app.secret_key = os.environ.get("SECRET_KEY")
