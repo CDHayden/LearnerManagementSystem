a4c467c422e0d175a40f6122b584b0630c817f3e
CDHayden <chayden50@gmail.com>
2021-01-23 17:04:59 +0000

    Update comments

--- lms/models/courses.py
+++ lms/models/courses.py
@@ -2,6 +2,8 @@ from typing import TypedDict, List
 from datetime import datetime
 
 #https://github.com/python/mypy/issues/5149
+#explains why these are structured like this
+
 class Attendance(TypedDict):
     name: str
     dates: List[datetime]
