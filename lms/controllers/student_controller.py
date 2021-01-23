c9fdca8f234de1c5fb37c6f73c49a38c23729b86
CDHayden <chayden50@gmail.com>
2021-01-23 18:21:38 +0000

    Move functionality from view to controller

--- lms/controllers/student_controller.py
+++ lms/controllers/student_controller.py
@@ -1,4 +1,6 @@
 from typing import List, Union
+import base64
+from flask import current_app
 
 from ..models.student_model import Student
 from database import mongo
@@ -10,7 +12,7 @@ def create_student_from_cursor(cursor) -> Student:
                 cursor['surname'],
                 cursor['profile_about'],
                 cursor['profile_image'],
-                cursor['classes'])
+                cursor['courses'])
     return s
 
 
@@ -36,3 +38,83 @@ def get_student_by_name(name: str, surname: str = None) -> Union[Student,List[St
 
     else:
         raise NameError(f"Student {name} {surname} could not be found.")
+
+def get_course_grade(student, course_name):
+    """Return student's grade for course_name"""
+    grade = student['courses'].get(course_name).get('grade')
+    return (course_name,grade)
+
+def get_course_subject(course_name):
+    """Return the subject that course belongs to"""
+    selected_course = mongo.db.courses.find_one({'name': course_name})
+    return selected_course['subject']
+
+def get_subjects_courses(student, course="all"):
+    """Return a dict of subjects and their courses for given student
+
+    Keyword arguements:
+    student -- The student you want subject and course information of
+    subject -- Specific courses from a single subject. Default is all.
+    """
+    # subjects_courses = {}
+    # for course_name in student.courses.keys():
+        # selected_subject = get_course_subject(course_name)
+        # if subject == "all" or subject == selected_subject:
+            # if selected_subject not in subjects_courses:
+                # subjects_courses[selected_subject] = []
+            # subjects_courses[selected_subject].append(course_name)
+
+    return student.courses.keys()
+
+def get_average_grade(grades):
+    """Return an average for a list of numbers"""
+    avg = sum(grades) / len(grades)
+    return avg
+
+def allowed_file(filename):
+    """Checks if the filename provided is permitted 
+
+    Permitted if it contains a '.' and the file extension is listed
+    in the ALLOWED_EXTENSIONS in the .env file
+    """
+
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in \
+        current_app.config['ALLOWED_EXTENSIONS']
+
+def update_student_profile(student_name,new_profile_data):
+    student = get_student_by_name(student_name)[0]
+    flashed_message = ""
+
+    if student.profile_about != new_profile_data['profile_about']:
+        mongo.db.users.update_one({'_id':student.id}, {'$set':{
+                'profile_about': new_profile_data['profile_about']
+            }})
+        if not flashed_message:
+            flashed_message = "Profile updated."
+
+    new_image = new_profile_data['profile_img']
+
+    if new_image and allowed_file(new_image.filename):
+        encoded_img = base64.b64encode(new_image.read()).decode()
+        filetype = new_image.filename.rsplit('.', 1)[1].lower()
+        img_data = f'data:image/{filetype};base64,{encoded_img}'
+        mongo.db.users.update_one({'_id':student.id}, {'$set':{
+                'profile_image': img_data
+            }})
+
+        if not flashed_message:
+            flashed_message = "Profile updated."
+
+    return flashed_message
+
+def generate_student_course_menu(student_name):
+    pass
+
+def load_subject_conent(student_name, subject):
+    pass
+
+
+def load_course_content(student_name, course):
+    pass
+
--- lms/views/student.py
+++ lms/views/student.py
@@ -1,113 +1,45 @@
-import base64
-
-from flask import Blueprint, render_template, jsonify, request, \
-    redirect, url_for, current_app, flash
-from werkzeug.utils import secure_filename
+from sys import stderr
+from flask import ( Blueprint, render_template, jsonify, request,  
+    redirect, url_for, flash )
 
 from database import mongo
+from ..controllers.student_controller import (get_student_by_name,
+       allowed_file, update_student_profile, get_subjects_courses)
 
 mod = Blueprint("student", __name__)
-
-def get_class_grade(student, class_name):
-    """Return student's grade for class_name"""
-    grade = student['classes'].get(class_name).get('grade')
-    return (class_name, grade)
-
-
-def get_class_subject(class_name):
-    """Return the subject that class_name belongs to"""
-    selected_class = mongo.db.classes.find_one({'name': class_name})
-    return selected_class['subject']
-
-
-def get_subjects_classes(student, subject="all"):
-    """Return a dict of subjects and their classes for given student
-
-    Keyword arguements:
-    student -- The student you want subject and class information of
-    subject -- Specific classes from a single subject. Default is all.
-    """
-    subjects_classes = {}
-    for class_name in student['classes'].keys():
-        selected_subject = get_class_subject(class_name)
-        if subject == "all" or subject == selected_subject:
-            if selected_subject not in subjects_classes:
-                subjects_classes[selected_subject] = []
-            subjects_classes[selected_subject].append(class_name)
-
-    return subjects_classes
-
-
-def get_average_grade(grades):
-    """Return an average for a list of numbers"""
-    sum = 0
-    for grade in grades:
-        sum += grade[1]
-    return sum / len(grades)
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in \
-        current_app.config['ALLOWED_EXTENSIONS']
 
 
 @mod.route('/')
 def student_index():
-    student = mongo.db.users.find_one({'forename': 'Chris'})
+    student = get_student_by_name("Chris Hayden")[0] 
     return render_template('student/index.html', student=student)
 
 
 @mod.route('/update_profile', methods=['POST'])
 def update_profile():
-    student = mongo.db.users.find_one({'forename': 'Chris'})
-    flashed_message = ""
-    if student['profile_about'] != request.form.get('profile_about'):
-        mongo.db.users.update_one({'forename': 'Chris'}, {'$set': {
-            'profile_about': request.form.get('profile_about')}})
-        flashed_message = "Profile saved."
+    data = {'profile_about':request.form.get('profile_about'),
+            'profile_img': request.files.get('profile_image')}
+    message = update_student_profile("Chris Hayden",data)
+    if message:
+        flash(message)
 
-    new_image = request.files.get("profile_image")
-    if new_image and allowed_file(new_image.filename):
-        encoded_img = base64.b64encode(new_image.read()).decode()
-        filetype = new_image.filename.rsplit('.', 1)[1].lower()
-        img_data = f'data:image/{filetype};base64,{encoded_img}'
-        mongo.db.users.update_one({'forename': 'Chris'}, {'$set': {
-            'profile_image': img_data
-        }})
-        flashed_message = "Profile saved."
-    flash(flashed_message)
     return redirect(url_for('student.student_index'))
 
 
-@mod.route('/classes')
-def student_classes():
-    student = mongo.db.users.find_one({'forename': 'Chris'})
-    menu_items = get_subjects_classes(student)
-    return render_template('student/classes.html',
+@mod.route('/courses')
+def student_courses():
+    student="chris"
+    menu_items = "no"
+    return render_template('student/courses.html',
                            student=student,
                            menu_items=menu_items)
 
 
-@mod.route('/_load_class_content')
-def load_class_content():
-    student = mongo.db.users.find_one({'forename': 'Chris'})
-    class_name = request.args.get('selected_class', "", type=str)
-    selected_class = student['classes'].get(class_name.strip())
-    return jsonify(class_name=class_name.strip(),
-                   grade=selected_class.get('grade'),
-                   day=selected_class.get('day'),
-                   start_time=selected_class.get('start_time'),
-                   finish_time=selected_class.get('finish_time'),
-                   attendance=selected_class.get('attendance'))
+@mod.route('/_load_course_content')
+def load_course_content():
+    pass
 
 
 @mod.route('/_load_subject_content')
 def load_subject_content():
-    student = mongo.db.users.find_one({'forename': 'Chris'})
-    subject = request.args.get('subject', "", type=str).strip()
-    student_classes = get_subjects_classes(student, subject)
-    grades = [get_class_grade(student, current_class)
-              for current_class in student_classes.get(subject)]
-    average_grade = get_average_grade(grades)
-    return jsonify(grades=grades, average_grade=average_grade)
+    pass
