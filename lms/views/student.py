b6add13bebaadd0cfad5fd16ee5c03e5d2ce1010
CDHayden <chayden50@gmail.com>
2021-01-25 14:19:48 +0000

    Get selected user ID from session variable

--- lms/views/student.py
+++ lms/views/student.py
@@ -1,25 +1,28 @@
 from sys import stderr
 from flask import ( Blueprint, render_template, jsonify, request,  
-    redirect, url_for, flash )
+    redirect, url_for, flash, session)
 
 from database import mongo
 from ..controllers.student_controller import (get_student_by_name,
-       allowed_file, update_student_profile, get_subjects_courses)
+       update_student_profile, get_student_by_id, get_subject_content)
 
 mod = Blueprint("student", __name__)
 
 
 @mod.route('/')
 def student_index():
-    student = get_student_by_name("Chris Hayden")[0] 
-    return render_template('student/index.html', student=student)
+    session['user'] = str(get_student_by_name("Chris Hayden")[0].id)
+    student = get_student_by_id(session['user'])
+    return render_template('student/index.html',
+    student=student)
 
 
 @mod.route('/update_profile', methods=['POST'])
 def update_profile():
+    student = get_student_by_id(session['user'])
     data = {'profile_about':request.form.get('profile_about'),
             'profile_img': request.files.get('profile_image')}
-    message = update_student_profile("Chris Hayden",data)
+    message = update_student_profile(student.id,data)
     if message:
         flash(message)
 
