81e8ce52375662aa208572f819613f5796a8f85b
CDHayden <chayden50@gmail.com>
2021-01-25 14:12:40 +0000

    Add event handlers for AJAX related links

--- lms/static/js/script.js
+++ lms/static/js/script.js
@@ -1,4 +1,3 @@
-
 /**
  * Called when a page has loaded and wants to apply the active class to the desired menu item.
  * Removes the active class from any other menu item.
@@ -10,14 +9,72 @@
  * @param elementId - Id of the menu item to apply active to
  */
 function selectActiveMenuItem(elementId, className) {
-    let list = document.querySelectorAll('#menuList > li');
+  let list = document.querySelectorAll('#menuList > li');
 
-    list.forEach(function (currentLink) {
-        if (currentLink.id == elementId) {
-            currentLink.classList.add(className);
-        }
-        else {
-            currentLink.classList.remove(className);
-        }
-    });
+  list.forEach(function (currentLink) {
+    if (currentLink.id == elementId) {
+      currentLink.classList.add(className);
+    } else {
+      currentLink.classList.remove(className);
+    }
+  });
 }
+
+/**
+ * Called when a link with the "course-content-link" class is clicked.
+ * Requests and loads information about the course via AJAX
+ *
+ * @param e - Event object passed in on click
+ * @param display - Element to display JSON data in
+ */
+const loadCourseContent = (e, displayID) => {
+  const course = e.srcElement.innerText;
+  const subject =
+    e.srcElement.parentElement.parentElement.parentElement.parentElement
+      .previousElementSibling.innerText;
+
+  const display = document.getElementById(displayID);
+
+  fetch('/student/_load_course_content', {
+    headers: { 'content-type': 'application/json; charset=utf-8' },
+    method: 'post',
+    body: JSON.stringify({
+      subject: subject,
+      course: course,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      display.innerText = '';
+      display.innerText = JSON.stringify(data);
+    });
+  e.preventDefault();
+};
+
+/**
+ * Called when a link with the "subject-content-link" class is clicked.
+ * Requests and loads information about the course via AJAX
+ *
+ * @param e - Event object passed in on click
+ * @param display - Element to display JSON data in
+ */
+const loadSubjectContent = (e, displayID) => {
+  const subject = e.srcElement.innerText;
+  const display = document.getElementById(displayID);
+
+  if (e.srcElement.attributes['aria-expanded'].nodeValue == 'true') {
+    fetch('/student/_load_subject_content', {
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      method: 'POST',
+      body: JSON.stringify({
+        subject: subject,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        display.innerText = '';
+        display.innerText = JSON.stringify(data);
+      });
+  }
+  e.preventDefault();
+};
