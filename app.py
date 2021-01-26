3693bbeb4268c1affe100e20d0bf6a2cc3876b0f
CDHayden <chayden50@gmail.com>
2021-01-26 10:46:43 +0000

    Provide default values for HOST and PORT

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from lms import app
 
 if __name__ == "__main__":
-    host = os.environ["HOST"]
-    port = os.environ["PORT"]
+    host = os.environ.get("HOST","127.0.0.1")
+    port = os.environ.get("PORT","5050")
 
     app.run(host, port)
