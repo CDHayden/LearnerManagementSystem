1602b9983ef18574c8a93f69559b043c6dc2eae8
CDHayden <chayden50@gmail.com>
2021-01-29 14:02:42 +0000

    Create routes for staff

--- lms/views/staff.py
+++ lms/views/staff.py
@@ -1,10 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
-from database import mongo
-
+import database
+from ..controllers.user_controller import get_user_by_id
 mod = Blueprint("staff", __name__)
 
 @mod.route('/')
 def staff_index():
-    return render_template('staff/index.html',
-                            user = mongo.db.users.find_one())
+    staff = get_user_by_id(session['user'])
+    return render_template('staff/index.html', user = staff)
+
+@mod.route('/my_classes')
+def staff_classes():
+    return "Classes"
+
+@mod.route('/manage_students')
+def manage_students():
+    return "students"
