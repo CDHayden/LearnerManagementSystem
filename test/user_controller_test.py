c0eb32941209b4ab15eb85d78cc7b3519f318ce7
CDHayden <chayden50@gmail.com>
2021-01-29 21:41:16 +0000

    Rename test suites

--- test/user_controller_test.py
+++ test/user_controller_test.py
@@ -7,9 +7,9 @@ from bson.objectid import ObjectId
 from lms import create_app
 import database
 from lms.models.user_model import User
-from lms.controllers.student_controller import (get_student_by_id, 
-create_student_from_cursor, get_subject_content, allowed_file,
-update_student_profile)
+from lms.controllers.user_controller import (get_user_by_id, 
+create_user_from_cursor, get_subject_content, allowed_file,
+update_user_profile)
 
 
 class TestStudentController(unittest.TestCase):
@@ -44,25 +44,25 @@ class TestStudentController(unittest.TestCase):
     def tearDown(self):
         database.mongo.db.users.drop()
 
-    def test_can_create_student_from_cursor(self):
+    def test_can_create_user_from_cursor(self):
         cursor = database.mongo.db.users.find_one({"forename":"Chris"})
-        student = create_student_from_cursor(cursor)
-        self.assertIsInstance(student,User)
+        user = create_user_from_cursor(cursor)
+        self.assertIsInstance(user,User)
 
-    def test_can_create_student_from_cursor_fails(self):
+    def test_can_create_user_from_cursor_fails(self):
         cursor = database.mongo.db.users.find_one({"forename":"Bob"})
-        student = create_student_from_cursor(cursor)
-        self.assertEqual({},student)
+        user = create_user_from_cursor(cursor)
+        self.assertEqual({},user)
 
 
-    def test_can_get_student_by_id(self):
-        student = get_student_by_id(ObjectId(b'testStudent1'))
-        self.assertIsInstance(student,User)
+    def test_can_get_user_by_id(self):
+        user = get_user_by_id(ObjectId(b'testStudent1'))
+        self.assertIsInstance(user,User)
         
-    def test_can_get_student_by_id_fails(self):
+    def test_can_get_user_by_id_fails(self):
         test_id = ObjectId(b'testStudent2')
-        student = get_student_by_id(test_id)
-        self.assertEqual({},student)
+        user = get_user_by_id(test_id)
+        self.assertEqual({},user)
 
     def test_can_get_subject_content(self):
         expected = {'avg_grade': 10.0, 'num_courses': 1}
@@ -82,24 +82,24 @@ class TestStudentController(unittest.TestCase):
         result = allowed_file("test.png")
         self.assertTrue(result)
 
-    def test_update_student_profile_only_change_about(self):
+    def test_update_user_profile_only_change_about(self):
         data = {'profile_about':'new about'}
-        value = update_student_profile(ObjectId(b'testStudent1'),data) 
-        student = get_student_by_id(ObjectId(b'testStudent1')) 
+        value = update_user_profile(ObjectId(b'testStudent1'),data) 
+        user = get_user_by_id(ObjectId(b'testStudent1')) 
 
         #check the profile was updated
         with self.subTest():
-            self.assertEqual("Profile updated.",value)
+            self.assertEqual(("Profile updated.","alert-success"),value)
 
         #check the profile_about was changed
         with self.subTest():
-            self.assertEqual('new about', student.profile_about)
+            self.assertEqual('new about', user.profile_about)
 
         #check the profile_image was not changed
         with self.subTest():
-            self.assertEqual('no',student.profile_image)
+            self.assertEqual('no',user.profile_image)
 
-    def test_update_student_profile_only_change_image(self):
+    def test_update_user_profile_only_change_image(self):
 
         # Info on mocking files and file reads taken from:
         # https://docs.python.org/3/library/unittest.mock.html#mocking-the-builtin-open-used-as-a-context-manager
@@ -111,19 +111,19 @@ class TestStudentController(unittest.TestCase):
         expected_img_return = 'data:image/png;base64,dGVzdA=='
 
         with patch('builtins.open', mock_open(read_data=b'test')):
-            value = update_student_profile(ObjectId(b'testStudent1'),data) 
+            value = update_user_profile(ObjectId(b'testStudent1'),data) 
 
-        student = get_student_by_id(ObjectId(b'testStudent1')) 
+        user = get_user_by_id(ObjectId(b'testStudent1')) 
 
         #check the profile was updated
         with self.subTest():
-            self.assertEqual("Profile updated.",value)
+            self.assertEqual(("Profile updated.","alert-success"),value)
 
         #check the profile_image was changed
         with self.subTest():
-            self.assertEqual(expected_img_return,student.profile_image)
+            self.assertEqual(expected_img_return,user.profile_image)
 
         #check the profile_about was not changed
         with self.subTest():
-            self.assertEqual('me', student.profile_about)
+            self.assertEqual('me', user.profile_about)
 
