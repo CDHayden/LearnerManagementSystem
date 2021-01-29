4673c92a68e8afbc54bb1b85894d74b6f8d97157
CDHayden <chayden50@gmail.com>
2021-01-29 14:04:47 +0000

    Use updated user functionality not student

--- lms/views/student.py
+++ lms/views/student.py
@@ -3,41 +3,40 @@ from flask import ( Blueprint, render_template, jsonify, request,
     redirect, url_for, flash, session)
 
 from database import mongo
-from ..controllers.student_controller import (get_student_by_name,
-       update_student_profile, get_student_by_id, get_subject_content)
+from ..controllers.user_controller import (get_user_by_name,
+       update_user_profile, get_user_by_id, get_subject_content)
 
 mod = Blueprint("student", __name__)
 
 
 @mod.route('/')
 def student_index():
-    student = get_student_by_id(session['user'])
+    student = get_user_by_id(session['user'])
     return render_template('student/index.html',student=student)
 
 
 @mod.route('/update_profile', methods=['POST'])
 def update_profile():
-    student = get_student_by_id(session['user'])
+    student = get_user_by_id(session['user'])
     data = {'profile_about':request.form.get('profile_about'),
             'profile_img': request.files.get('profile_image')}
-    message = update_student_profile(student.id,data)
-    if message:
-        flash(message)
+    message = update_user_profile(student.id,data)
+    flash(message[0],message[1])
 
     return redirect(url_for('student.student_index'))
 
 
 @mod.route('/courses')
 def student_courses():
-    student = get_student_by_id(session['user'])
+    student = get_user_by_id(session['user'])
     return render_template('student/courses.html',
-                           student=get_student_by_id(session['user']),
+                           student=get_user_by_id(session['user']),
                            menu_items=student.subjects)
 
 
 @mod.route('/_load_course_content', methods=['POST'])
 def load_course_content():
-    student = get_student_by_id(session['user'])
+    student = get_user_by_id(session['user'])
     subject = request.json['subject']
     course = request.json['course']
     content = student.subjects[subject][course];
