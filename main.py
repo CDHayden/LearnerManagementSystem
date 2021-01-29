fc69a62029fbe9b3c81abab20d90f2a86e1a3e1f
CDHayden <chayden50@gmail.com>
2021-01-29 10:32:51 +0000

    Fix imports

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from lms import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    host = os.environ.get("HOST", "0.0.0.0")
-    port = os.environ.get("PORT", "5000")
+    host = environ.get("HOST", "0.0.0.0")
+    port = environ.get("PORT", "5000")
     app.run(host, port)
