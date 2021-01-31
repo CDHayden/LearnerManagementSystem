fad4c44a1c77f0faf9474af6c16d7671db6764b3
CDHayden <chayden50@gmail.com>
2021-01-31 20:59:04 +0000

    Add option to populate db with dummy data

--- lms/views/home.py
+++ lms/views/home.py
@@ -1,6 +1,7 @@
 from flask import (Blueprint, render_template, request, redirect,
 flash, session, url_for)
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash
 
 import database
 from ..controllers.home_controller import ( log_user_in, 
@@ -43,9 +44,9 @@ def signout():
 
     return redirect(url_for('home.index'))
 
-@mod.app_errorhandler(Exception)
-def error_handler(e):
-    return render_template('home/error.html')
+# @mod.app_errorhandler(Exception)
+# def error_handler(e):
+    # return render_template('home/error.html')
 
 
 @mod.route('/profile/<username>')
@@ -54,3 +55,64 @@ def public_profile(username):
     if found_user:
         user = get_returnable_user(found_user)
     return render_template('home/profile.html', profile = user)
+
+@mod.route('/populate_db')
+def populate_db():
+    database.mongo.db.users.insert_many([
+            {
+            '_id':ObjectId(),
+            'username':'student1',
+            'password':generate_password_hash('test'),
+            'is_teacher':False,
+            'forename':'chris',
+            'surname':'hayden',
+            'profile_about': 'I need to update my profile',
+            'profile_image': 'none',
+            'subjects':[{'subject':'spanish','course':'speaking','grade':30}]
+            }, 
+            {
+            '_id':ObjectId(),
+            'username':'student2',
+            'password':generate_password_hash('test'),
+            'is_teacher':False,
+            'forename':'bob',
+            'surname':'loblaw',
+            'profile_about': 'I need to update my profile',
+            'profile_image': 'none',
+            'subjects':[{'subject':'spanish','course':'listening','grade':20},{'subject':'spanish','course':'speaking','grade':30}]
+            },
+            {
+            '_id':ObjectId(),
+            'username':'student3',
+            'password':generate_password_hash('test'),
+            'is_teacher':False,
+            'forename':'miriam',
+            'surname':'vilar',
+            'profile_about': 'I need to update my profile',
+            'profile_image': 'none',
+            'subjects':[{'subject':'english','course':'speaking','grade':100},{'subject':'german','course':'writing','grade':20}]
+            },
+            {
+            '_id':ObjectId(),
+            'username':'teacher1',
+            'password':generate_password_hash('test'),
+            'is_teacher':True,
+            'forename':'Juan',
+            'surname':'Carlos',
+            'profile_about': 'I need to update my profile',
+            'profile_image': 'none',
+            'subjects':[{'subject':'spanish','course':'speaking','grade':0},{'subject':'spanish','course':'listening','grade':0}]
+            },
+            {
+            '_id':ObjectId(),
+            'username':'teacher1',
+            'password':generate_password_hash('test'),
+            'is_teacher':True,
+            'forename':'Saul',
+            'surname':'Goodman',
+            'profile_about': 'I need to update my profile',
+            'profile_image': 'none',
+            'subjects':[{'subject':'english','course':'speaking','grade':0},{'subject':'german','course':'writing','grade':0}]
+            }]
+            )
+    return "Done"
